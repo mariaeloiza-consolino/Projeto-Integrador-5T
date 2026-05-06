87374474dbf1c597e3e864427930ea305226bc82
Maria Eloiza Marques Consolino <165097868+mariaeloiza-consolino@users.noreply.github.com>
2026-05-06 23:47:19 +0000

    exercicios 1

---
+++ my-app/src/components/CardSimples.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './CardSimples.css';
+
+function CardSimples (){
+    return (
+    <div className='enfeite'>
+        <h2> Nome do Aluno </h2>
+        <h3> Curso do Aluno </h3>
+        <button> Ver perfil </button>
+    </div>
+    
+    )
+}
+
+export default CardSimples;
